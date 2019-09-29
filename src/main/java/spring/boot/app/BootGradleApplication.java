cde6c80354a0c78206f31ffb5f1afa03c0bd8f84
AFSAR EQUEBAL <afsarequebal@gmail.com>
2019-09-29 23:38:30 +0530

    spring boot app

---
+++ src/main/java/spring/boot/app/BootGradleApplication.java
@@ -0,0 +1,14 @@
+package spring.boot.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootGradleApplication.class, args);
+		System.out.println("Hello r world1");
+	}
+}
